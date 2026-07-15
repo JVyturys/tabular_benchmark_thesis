d692de15880b915622779a998f767de7a489d146
JVyturys <vyturysjaunius@gmail.com>
2026-07-15 14:36:21 +0200

    update module: finalize number_obs_cntry

--- src/exploration/raw_data_and_panel/number_obs_cntry.py
+++ src/exploration/raw_data_and_panel/number_obs_cntry.py
@@ -1,6 +1,7 @@
 ##################################################
 '''
-Find out the number of observations per country in the final panel.
+Find out the number of observations per entity & 
+the total observation per country in the final panel.
 '''
 ##################################################
 
@@ -15,5 +16,27 @@ db = wrds.Connection(wrds_username = lc.wrds_log)
 data_path = con.FINAL_PANEL_PARQUET
 output_path = con.PROJECT_ROOT / 'data'
 results_path = con.RESULTS_DIR/ 'joined_panel'
+
+# Load data
 df = pd.read_parquet(data_path)
 geo = pd.read_csv(con.PROJECT_ROOT / "data" / "raw" / "geo_ref.csv")
+
+# create table: observations & country ID
+df_orgs = df[['orgpermid']] # entitiy IDs 
+geo_cntry = geo[['orgpermid','lvl5isocntry']] # iso-country codes
+
+# merge data sets
+df_geo_merge = df_orgs.merge(geo_cntry, on='orgpermid', how='left')
+
+# count observations per entity
+obs_per_ent = df_geo_merge.value_counts()
+
+# count observation per entity
+df_obs_cntry = df_geo_merge['lvl5isocntry'] # isolating countries
+obs_per_cntry = df_obs_cntry.value_counts()
+
+
+
+obs_per_ent.to_csv("obs_per_ent.csv", index=True)
+obs_per_cntry.to_csv("obs_per_cntry.csv", index=True)
+
