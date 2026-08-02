95871fbb53c163bce621e700696b020351ec0d77
JVyturys <vyturysjaunius@gmail.com>
2026-08-02 10:28:46 +0200

    add exp: num. of clusters per entity thin region

--- src/exploration/final_panel/determine_geoID_dist.py
+++ src/exploration/final_panel/determine_geoID_dist.py
@@ -7,25 +7,53 @@ purpose: determine the observations per region and the
         number of entities per region
 output:
         Observations per Region - Share
-                    entities  observations  obs / entity
-        lvl3permid                                      
-        100277           151          1734         11.48
-        100090           124          1412         11.39
-        110000             2            22         11.00
-        100334           925          8967          9.69
-        103384           452          4364          9.65
-        100024           668          6352          9.51
-        100279           325          3036          9.34
-        100089          2743         25243          9.20
-        100219           609          5511          9.05
-        100223          1396         12261          8.78
-        103401           459          3360          7.32
-        100060             5            34          6.80
-        100276          1055          6947          6.58
-        100218            88           541          6.15
-        100278           693          4165          6.01
-        100087            11            62          5.64
-        100332             9            38          4.22
+                    entities  observations  obs / entity    70%   30%
+        lvl3permid                                                   
+        100277           151          1734         11.48   1214   520
+        100090           124          1412         11.39    988   424
+        110000             2            22         11.00     15     7
+        100334           925          8967          9.69   6277  2690
+        103384           452          4364          9.65   3055  1309
+        100024           668          6352          9.51   4446  1906
+        100279           325          3036          9.34   2125   911
+        100089          2743         25243          9.20  17670  7573
+        100219           609          5511          9.05   3858  1653
+        100223          1396         12261          8.78   8583  3678
+        103401           459          3360          7.32   2352  1008
+        100060             5            34          6.80     24    10
+        100276          1055          6947          6.58   4863  2084
+        100218            88           541          6.15    379   162
+        100278           693          4165          6.01   2916  1250
+        100087            11            62          5.64     43    19
+        100332             9            38          4.22     27    11
+
+        Numer of clusters for small-sized regions:
+        lvl3permid  cluster_key
+        100060      5000063281     1
+                    5000435671     2
+                    5000462835     1
+                    5052145845     1
+        100087      4295889219     1
+                    4295894791     1
+                    4296813461     1
+                    4298007709     1
+                    4298008740     1
+                    5000025223     1
+                    5000026125     1
+                    5000758280     1
+                    5001208962     1
+                    5042239845     1
+                    5053943433     1
+        100332      4295885261     1
+                    4295885276     1
+                    4295888681     2
+                    4296534101     1
+                    4296901683     1
+                    4297161004     1
+                    4298000881     1
+                    5036776503     1
+        110000      4295857491     1
+                    4297636264     1
 
 '''
 ##################################################
@@ -37,42 +65,37 @@ import math
 # load data
 df = pd.read_parquet(con.PANEL) 
 ref_geo = pd.read_parquet(con.REF_GEOGRAPHY)
+clust_key = pd.read_parquet(con.REF_CLUSTER_KEYS)
 
 # extract org IDs
-df_ents_per_region = df[['orgpermid']].drop_duplicates()
-print(f'''\nnmb. of unique entities in panel: {len(df_ents_per_region)}''')
+df_ents = df[['orgpermid']].drop_duplicates()
+print(f'''\nnmb. of unique entities in panel: {len(df_ents)}''')
 
 # merge with geo IDs
-df_ents_per_region = df_ents_per_region.merge(ref_geo, how='left', on='orgpermid')
+df_ents = df_ents.merge(ref_geo, how='left', on='orgpermid')
 
 # group by region to get entities per region
 print(f'''\nentities per region:''')
-print(f'''{df_ents_per_region.groupby('lvl3permid', dropna=False).size().sort_values(ascending=False)}''')   
+print(f'''{df_ents.groupby('lvl3permid', dropna=False).size().sort_values(ascending=False)}''')   
 
 # extract org IDs for observation count
-df_obs_per_region = df[['orgpermid']]
-print(f'''\nnmb. of observations in panel: {len(df_obs_per_region)}''')
+df_obs = df[['orgpermid']]
+print(f'''\nnmb. of observations in panel: {len(df_obs)}''')
 
 # merge with geo IDs
-df_obs_per_region = df_obs_per_region.merge(ref_geo, how='left', on='orgpermid')
+df_obs = df_obs.merge(ref_geo, how='left', on='orgpermid')
 
 # group by region to get entities per region
 print(f'''\nobs per region:''')
-print(f'''{df_obs_per_region.groupby('lvl3permid', dropna=False).size().sort_values(ascending=False)}''')   
+print(f'''{df_obs.groupby('lvl3permid', dropna=False).size().sort_values(ascending=False)}''')   
 
-# build grouped region tables side-by-side
-grp_ents_per_region = df_ents_per_region.groupby('lvl3permid', dropna=False).size()
-grp_obs_per_region = df_obs_per_region.groupby('lvl3permid', dropna=False).size()
+# build grouped region tables side-by-sidegrp_ents_per_region
+grp_ents_per_region = df_ents.groupby('lvl3permid', dropna=False).size()
+grp_obs_per_region = df_obs.groupby('lvl3permid', dropna=False).size()
 regional_obs_per_ent = grp_obs_per_region.div(grp_ents_per_region, level='lvl3permid')
 
 split_30 = round(grp_obs_per_region*0.3).astype('int64')
 split_70 = round(grp_obs_per_region*0.7).astype('int64')
-
-split_20 = round(grp_obs_per_region*0.2).astype('int64')
-split_80 = round(grp_obs_per_region*0.8).astype('int64')
-
-split_25 = round(grp_obs_per_region*0.25).astype('int64')
-split_75 = round(grp_obs_per_region*0.75).astype('int64')
 
 df_reg_obs_per_ent = pd.DataFrame({
     "entities" : grp_ents_per_region,
@@ -80,11 +103,18 @@ df_reg_obs_per_ent = pd.DataFrame({
     "obs / entity" : round(regional_obs_per_ent, 2),
     "70%": split_70,
     "30%": split_30,
-    "80%": split_80,
-    "20%": split_20,
-    "75%": split_75,
-    "25%": split_25
 })
 
 print(f'''Observations per Region - Share''')
 print(f'''{df_reg_obs_per_ent.sort_values('obs / entity', ascending=False)}''')
+
+# merge wit cluster keys
+df_clust = clust_key[['orgpermid', 'cluster_key']]
+
+df_ents = df_ents.merge(df_clust, how='left', on='orgpermid')
+print(f'''df_ents''')
+print(df_ents)
+
+small_regions = [110000,100060, 100087, 100332]
+grp_ents_clust = df_ents.query('lvl3permid.isin(@small_regions)').groupby(['lvl3permid', 'cluster_key']).size()
+print(grp_ents_clust)
