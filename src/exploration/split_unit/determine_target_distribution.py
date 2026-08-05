a94484ebcdb206a160ee0981410f92a333a697b2
JVyturys <vyturysjaunius@gmail.com>
2026-08-05 17:11:41 +0200

    add exp module: target distribution

---
+++ src/exploration/split_unit/determine_target_distribution.py
@@ -0,0 +1,13 @@
+##################################################
+'''
+src.exploration.split_unit.determine_target_distribution
+
+
+input: panel.parquet
+purpose: determine shape of the target variable globally
+        and per region.
+
+output:
+
+'''
+##################################################
