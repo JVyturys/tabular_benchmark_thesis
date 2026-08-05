15904cc768494be78dd39fa7a0a1737c53865872
JVyturys <vyturysjaunius@gmail.com>
2026-08-05 11:46:35 +0200

    adjust module description

--- src/exploration/split_unit/determine_per_region_coverage_growth.py
+++ src/exploration/split_unit/determine_per_region_coverage_growth.py
@@ -2,12 +2,10 @@
 '''
 src.exploration.split_unit.determine_per_region_coverage_growth
 
-input: ref_cluster_keys.parquet
-purpose: determine the number of clusters based on the cluster key
-        assignment
-output: clusters-based on ultpar: 8865
-        clusters-based on immpar: 847
-        singleton-clusters: 3
+input: panel.parquet, ref_geo.parquet
+purpose: determine the rating coverage development in total and 
+        per region.
+output: 
 '''
 ##################################################
 
@@ -21,3 +19,5 @@ ref_geo = pd.read_parquet(con.REF_GEOGRAPHY)
 # slice relevant vars
 df_org = panel[['orgpermid', 'year']]
 df_geo = ref_geo[['orgpermid', 'lvl3permid']]
+
+# extract entry year per orgpermid
