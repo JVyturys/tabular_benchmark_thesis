01adc8d774e20a8276649f184e701598e838f93a
JVyturys <vyturysjaunius@gmail.com>
2026-08-05 18:38:51 +0200

    update docstring

--- src/exploration/split_unit/determine_target_distribution.py
+++ src/exploration/split_unit/determine_target_distribution.py
@@ -5,8 +5,9 @@ src.exploration.split_unit.determine_target_distribution
 
 input: panel.parquet
 purpose: determine shape of the target variable globally
-        and per region.
-
+        and per region,
+        determine regional shift against reference region
+        using the Wasserstein-Distance as a distance metric
 output:
 
 '''
