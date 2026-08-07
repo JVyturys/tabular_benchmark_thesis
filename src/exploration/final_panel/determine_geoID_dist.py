21abc9d03991a706cc2e2ea7429aa08b5e4e79c1
JVyturys <vyturysjaunius@gmail.com>
2026-08-07 13:48:20 +0200

    add validation size estimate for 60/10/30

--- src/exploration/final_panel/determine_geoID_dist.py
+++ src/exploration/final_panel/determine_geoID_dist.py
@@ -4,28 +4,29 @@ src.exploration.final_panel.determine_geoID_dist
 
 input: panel.parquet, ref_geo_table.parquet
 purpose: determine the observations per region and the 
-        number of entities per region
+        number of entities per region,
+        approximate split sizes
 output:
         Observations per Region - Share
-                    entities  observations  obs / entity    70%   30%
-        lvl3permid                                                   
-        100277           151          1734         11.48   1214   520
-        100090           124          1412         11.39    988   424
-        110000             2            22         11.00     15     7
-        100334           925          8967          9.69   6277  2690
-        103384           452          4364          9.65   3055  1309
-        100024           668          6352          9.51   4446  1906
-        100279           325          3036          9.34   2125   911
-        100089          2743         25243          9.20  17670  7573
-        100219           609          5511          9.05   3858  1653
-        100223          1396         12261          8.78   8583  3678
-        103401           459          3360          7.32   2352  1008
-        100060             5            34          6.80     24    10
-        100276          1055          6947          6.58   4863  2084
-        100218            88           541          6.15    379   162
-        100278           693          4165          6.01   2916  1250
-        100087            11            62          5.64     43    19
-        100332             9            38          4.22     27    11
+                entities  observations  obs / entity    70%   30%       val_60/10/30
+        lvl3permid                                                                 
+        100277           151          1734         11.48   1214   520           170
+        100090           124          1412         11.39    988   424           138
+        110000             2            22         11.00     15     7             2
+        100334           925          8967          9.69   6277  2690           879
+        103384           452          4364          9.65   3055  1309           428
+        100024           668          6352          9.51   4446  1906           622
+        100279           325          3036          9.34   2125   911           298
+        100089          2743         25243          9.20  17670  7573          2474
+        100219           609          5511          9.05   3858  1653           540
+        100223          1396         12261          8.78   8583  3678          1202
+        103401           459          3360          7.32   2352  1008           329
+        100060             5            34          6.80     24    10             3
+        100276          1055          6947          6.58   4863  2084           681
+        100218            88           541          6.15    379   162            53
+        100278           693          4165          6.01   2916  1250           408
+        100087            11            62          5.64     43    19             6
+        100332             9            38          4.22     27    11             4
 
         Numer of clusters for small-sized regions:
         lvl3permid  cluster_key
@@ -95,6 +96,7 @@ regional_obs_per_ent = grp_obs_per_region.div(grp_ents_per_region, level='lvl3pe
 
 split_30 = round(grp_obs_per_region*0.3).astype('int64')
 split_70 = round(grp_obs_per_region*0.7).astype('int64')
+split_val_14p = round(grp_obs_per_region*0.7*0.14).astype('int64')
 
 df_reg_obs_per_ent = pd.DataFrame({
     "entities" : grp_ents_per_region,
@@ -102,6 +104,7 @@ df_reg_obs_per_ent = pd.DataFrame({
     "obs / entity" : round(regional_obs_per_ent, 2),
     "70%": split_70,
     "30%": split_30,
+    "val_60/10/30":split_val_14p
 })
 
 print(f'''Observations per Region - Share''')
