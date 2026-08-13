711609858f31ab442a3fcccdea649c6f53cf495a
JVyturys <vyturysjaunius@gmail.com>
2026-08-13 16:02:19 +0200

    add outer split logic & outer deviation report

--- src/production/from_final_panel/build_split_partitions.py
+++ src/production/from_final_panel/build_split_partitions.py
@@ -63,25 +63,28 @@ assert len(df_split) == pre_merge_len, "lenght of df_split changed during merge 
 # perform index partition allocation
 # # initiate random number generator and set seed
 rng = np.random.default_rng(seed=con.SEED)
+
 # # create partition key
-df_partition = df_org.copy()
+df_partition = df_split[['orgpermid', 'cluster_key', 'lvl3permid', 'cluster_home']]
 df_partition['partition'] = np.nan
 
+# # set up per-region deviation report
+deviation_report = {} 
+
 # # test-train idex split 
-# for current_region in regs:
-test = True
-if test == True:
+for current_region in regs:
+
     # select rows of one region
-    df_current = df_split.query('cluster_home == 100087')
+    df_current = df_split.query('cluster_home == @current_region')
     df_current['test_train'] = np.nan 
 
     # create ordered list of clusters based on cluster-sample-size in current region
     counts = df_current['cluster_key'].value_counts().rename_axis('cluster_key').reset_index(name='freq')
     counts['rand_weight'] = rng.random(len(counts))
     ordered_clusters = counts.sort_values(
-    by=['freq', 'rand_weight'], 
-    ascending=[False, True]
-)['cluster_key'].tolist()
+                                        by=['freq', 'rand_weight'], 
+                                        ascending=[False, True]
+                                        )['cluster_key'].tolist()
 
     # determine partition goals for current regions; rps
     synth_reg_size = df_current.groupby('cluster_home').size().sum()
@@ -90,18 +93,36 @@ if test == True:
     test_size_current = len(df_current.query('test_train=="test"'))
     train_size_current = len(df_current.query('test_train=="train"'))
 
-    # determine distance to partition goal
-    dist_train = 1 - train_size_current / train_size_goal
-    dist_test = 1 - test_size_current / test_size_goal
+    # assign each cluster to a partition
+    for parent in ordered_clusters:
+        test_size_current = len(df_current.query('test_train=="test"'))
+        train_size_current = len(df_current.query('test_train=="train"'))
 
-    # direct cluster to partition 
-    if dist_train > dist_test:
-        pass
-    elif dist_train < dist_train:
-        pass
+        # determine distance to partition goal
+        dist_train = 1 - train_size_current / train_size_goal
+        dist_test = 1 - test_size_current / test_size_goal
+
+        # direct cluster to partition 
+        idx = df_current['cluster_key'] == parent
+
+        if dist_train > dist_test:
+            df_current.loc[idx, 'test_train'] = 'train'
+            
+        elif dist_train < dist_test: # Fixed typo: was dist_train < dist_train
+            df_current.loc[idx, 'test_train'] = 'test'
+            
+        elif dist_train - dist_test == con.DIST_TOL: 
+            df_current.loc[idx, 'test_train'] = 'test'
+
+        # save allocation outside the loop 
+        merged = df_partition.merge(df_current, on=['orgpermid', 'cluster_key', 'lvl3permid', 'cluster_home'], how='left')
+        df_partition['partition'] = merged['test_train'].fillna(df_partition['partition'])
+
+    # add deviation entry for current region to report
+    current_deviation = len(df_current[['test_train']].query('test_train=="test"'))/synth_reg_size-con.TEST_SHARE
+    deviation_report.update({current_region:current_deviation})
+       
+df_deviation_rep = pd.DataFrame.from_dict(deviation_report, orient="index", columns=["deviation"])
+print(df_deviation_rep)
 
     
-
-
-
-# for region in df_split['cluster_home']:
