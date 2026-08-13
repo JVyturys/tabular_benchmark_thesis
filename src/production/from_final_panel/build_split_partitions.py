49e51f1a769f30a13e38d862654b518a2440fda3
JVyturys <vyturysjaunius@gmail.com>
2026-08-13 11:27:41 +0200

    add cluster-home regions to main dframe

--- src/production/from_final_panel/build_split_partitions.py
+++ src/production/from_final_panel/build_split_partitions.py
@@ -13,16 +13,42 @@ output: idx-sets for each partition
 
 import pandas as pd
 import config as con
+import math 
 
 # load data
 panel = pd.read_parquet(con.PANEL)
 geo = pd.read_parquet(con.REF_GEOGRAPHY)
 cluster = pd.read_parquet(con.REF_CLUSTER_KEYS)
 
+# load non-degenerate region list
+regs =[*con.TIER1_REGS, *con.TIER2_REGS]
+
 # merge data
 df_org =panel[['orgpermid']]
 df_geo = geo[['orgpermid', 'lvl3permid']]
 df_clust = cluster[['orgpermid', 'cluster_key']]
-df_split = df_org.merge(df_org, on='orgpermid', how='left')
 df_split = df_org.merge(df_geo, on='orgpermid', how='left')
-df_split = df_org.merge(df_clust, on='orgpermid', how='left')
+df_split = df_split.merge(df_clust, on='orgpermid', how='left')
+df_split = df_split.query('lvl3permid.isin(@regs)')
+
+# append cluster-home-region columns
+##  define region-cluster distribution h(cluster_key_i, lvl3permid_j)
+biv_dist_clgeo = df_split[['lvl3permid', 'cluster_key']].groupby(['cluster_key', 'lvl3permid'], dropna=False).size()
+df_biv_clgeo = pd.DataFrame({
+    "n_observations":biv_dist_clgeo
+})
+
+## determine cluster-regions with highest frequency of observations per cluster
+idx_max = df_biv_clgeo.groupby('cluster_key', dropna=False)['n_observations'].idxmax()
+max_obs = df_biv_clgeo.loc[idx_max]
+df_max_obs = max_obs.reset_index() 
+df_max_obs = df_max_obs.rename(columns={'lvl3permid':'cluster_home'})
+print(df_max_obs)
+df_max_obs = df_max_obs[['cluster_key', 'cluster_home']]
+
+# append home-region information to split data frame
+df_split = df_split.merge(df_max_obs, on="cluster_key")
+
+# determine share of observations that have a from physical lvl3permid distinct cluster home
+off_home_obs_share = len(df_split[df_split['lvl3permid']!=df_split['cluster_home']])/len(df_org)
+
