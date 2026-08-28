5e3a7133a6f2fbd0493dba35ad41f6f9cb983b25
JVyturys <vyturysjaunius@gmail.com>
2026-08-28 16:29:42 +0200

    factor out slicing logic elelement from helper

--- stageguard.py
+++ stageguard.py
@@ -66,10 +66,9 @@ class Gatekeeper():
         stage_data = self._preprocessed_data[self._preprocessed_data['partition'].isin(partitions)]
         if stage in {None,1,2,3}:
             geo_ID = None
-            stage_data = stage_data.drop(columns=['orgpermid', 'lvl3permid', 'partition'])
         elif stage in {4}:
             geo_ID = stage_data['lvl3permid']
-            stage_data = stage_data.drop(columns=['orgpermid', 'partition', 'lvl3permid'])
+        stage_data = stage_data.drop(columns=['orgpermid', 'partition', 'lvl3permid'])
         stage_X = stage_data.drop(columns=['esg_combined_score'])
         stage_y = stage_data[['esg_combined_score']]
         return stage_X, stage_y, geo_ID
@@ -164,7 +163,7 @@ class Gatekeeper():
 
         # slice data
         X,y,geo_ID = self._slice_data({'4'}, self._stage)
-        print(f"stage 4 data provided, X - {X.shape}, y - {y.shape}")
+        print(f"stage 4 data provided, X - {X.shape}, y - {y.shape}, geo-ID - {geo_ID.shape}")
         return X, y, geo_ID
 ### ---------------------------------------------------------
 
