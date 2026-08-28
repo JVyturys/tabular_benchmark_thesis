3ab12c15515d947e4c7cc614de7284c023d049ae
JVyturys <vyturysjaunius@gmail.com>
2026-08-28 15:16:50 +0200

    fix class logic and several naming bugs

--- stageguard.py
+++ stageguard.py
@@ -2,7 +2,7 @@ import numpy as np
 import pandas as pd
 import config as con
 
-class Gatekeeper:
+class Gatekeeper():
     '''
     Provide designated, preprocessed data slices to distinct model phases.
     Data provided depends on model training phase (training on fit, validating, training on train, testing) and
@@ -12,10 +12,14 @@ class Gatekeeper:
 
 ### define initializers  --------------------------------------------------------- 
 
-    def __init__(self):
-        # initialize stage parameters
+    def __init__(self, model):
         print(f'\n\n[+][+][+] intializing data gatekeeper [+][+][+]\n\n')
-        self._stage = None        
+        # initialize stage parameters
+        self.model = model
+        self._stage = None   
+        self._stage_two = None
+
+        assert self.model in {'ICL', 'nICL'}, f"expected model classes: ICL or nICL, received {self.model}"
 
         # load data and parameters
         self.panel = pd.read_parquet(con.PANEL)
@@ -31,14 +35,14 @@ class Gatekeeper:
         self._preprocessed_data = self._preprocessing()
 
         # print intitalization convergence
-        print(f'\n \n [-] data gatekeeper initialized - proceed with stage call [-]\n \n')
+        print(f'\n \n [-] data gatekeeper initialized for model class {self.model} [-] \n [-] proceed with stage call [-]\n \n')
 
     def _preprocessing(self):
         '''
         Scale and impute dataset.
         '''
         feature_names = self.pre_processing_constants['variable']
-        variables_to_keep = [*feature_names, 'esg_combined_score', 'lvl3permid', 'partition']
+        variables_to_keep = [*feature_names, 'esg_combined_score', 'orgpermid', 'lvl3permid', 'partition', ]
         pre_processed_data = self.merged_data[variables_to_keep].copy()
         constants = self.pre_processing_constants.set_index('variable')
         print(f"initializing data preprocessing - raw data shape: {self.merged_data.shape}")
@@ -52,12 +56,18 @@ class Gatekeeper:
         return pre_processed_data
 
     def _require(self, expected):
-        if self._stage != expected:
-            raise RuntimeError(f"needs {expected}, saw {self._stage}")
+        if self._stage not in expected:
+                raise RuntimeError(f"needs stage {expected}, saw {self._stage}")
+
+    def _require_class(self, expected):
+        if self.model not in expected:
+            raise RuntimeError(f"needs model class {expected}, saw {self.model}")
+    
+
 
 ### define methods ---------------------------------------------------------
 
-    def stage_one_data(self, model, stage = 0):
+    def stage_one_data(self):
         '''
         Output data for stage 1 model phase.
         Output:
@@ -66,24 +76,21 @@ class Gatekeeper:
             - y (n,1), native in [0,1], NaN-free (by pull design)
         Conditions: stage == None OR previous_stage == 2, model == nICL
         '''
-        # initialize
-        if stage == 0:
-            stage = self._stage
 
         # check conditions
-        assert model == 'nICL', f"expected model class: nICL, received {model}"
-        assert stage == None or stage == 2, f"expected stages: None OR 2, received: {stage}"
+        self._require({None, 2})
+        self._require_class({'nICL'})
 
         # slice data
-        stage_data = self.preprocessed_data.query('partition==fit')
+        stage_data = self._preprocessed_data.query('partition=="fit"')
         stage_data = stage_data.drop(columns=['orgpermid', 'lvl3permid', 'partition'])
         self._stage = 1
-        print(f"stage 1 data provided - (n,X+y)= {len(stage_data)}")
+        print(f"stage 1 data provided - (n,X+y)= {stage_data.shape}")
         return stage_data
 
 ### ---------------------------------------------------------
 
-    def stage_two_data(self, model, stage = 0):
+    def stage_two_data(self):
         '''
         Output data for stage 2 model phase.
         Output:
@@ -92,24 +99,22 @@ class Gatekeeper:
             - y (n,1), native in [0,1], NaN-free (by pull design)
         Conditions: previous_stage == 1, model class == 'nICL' 
         '''
-                # initialize
-        if stage == 0:
-            stage = self._stage
+        self._stage_two = True
 
         # check conditions
-        assert model == 'nICL', f"expected model class: nICL, received {model}"
-        self._require(1)
+        self._require({1})
+        self._require_class({'nICL'})
 
         # slice data
-        stage_data = stage_data.query('partition==val')
-        stage_data = self.preprocessed_data.drop(columns=['orgpermid', 'lvl3permid', 'partition'])
+        stage_data = self._preprocessed_data.query('partition=="val"')
+        stage_data = stage_data.drop(columns=['orgpermid', 'lvl3permid', 'partition'])
         self._stage = 2
-        print(f"stage 2 data provided - (n,X+y)= {len(stage_data)}")
+        print(f"stage 2 data provided - (n,X+y)= {stage_data.shape}")
         return stage_data
 
 ### ---------------------------------------------------------
 
-    def stage_three_data(self, model, stage=0):
+    def stage_three_data(self):
         '''
         Output data for stage 3 model phase.
         Output:
@@ -118,45 +123,39 @@ class Gatekeeper:
             - y (n,1), native in [0,1], NaN-free (by pull design)
         Conditions: previous_stage == 2, model class == 'nICL' 
         '''
-        #initialize
-        if stage == 0:
-            stage = self._stage
 
         # check conditions
-        assert model == 'ICL' or model == 'nICL', f"expected model class ICL or nICL, received {model}"
-        self._require(2)
+        if self.model== 'nICL':
+            self._require({2})
+        elif self.model=='ICL':
+            self._require(None)
 
         # slice data
-        stage_data = stage_data.query('partition==val | partition==fit')
-        stage_data = self.preprocessed_data.drop(columns=['orgpermid', 'lvl3permid', 'partition'])
+        stage_data = self._preprocessed_data.query('partition=="val" | partition=="fit"')
+        stage_data = stage_data.drop(columns=['orgpermid', 'lvl3permid', 'partition'])
         self._stage = 3
-        print(f"stage 3 data provided - (n,X+y)= {len(stage_data)}")
+        print(f"stage 3 data provided - (n,X+y)= {stage_data.shape}")
         return stage_data
 
 ### ---------------------------------------------------------
 
-    def stage_four_data(self, model, stage=0):
+    def stage_four_data(self):
         '''
         Output data for stage 4 model phase.
         Output:
-            - partition: tset  
+            - partition: test  
             - X (n,125), preprocessed.
             - y (n,1), native in [0,1], NaN-free (by pull design)
         Conditions: previous_stage == 3 
         '''
-        #initialize
-        if stage == 0:
-            stage = self._stage
-
         # check conditions
-        assert model == 'ICL' or model == 'nICL', f"expected model class ICL or nICL, received {model}"
-        self._require(3)
+        self._require({3})
 
         # slice data
-        stage_data = stage_data.query('partition==test')
-        stage_data = self.preprocessed_data.drop(columns=['orgpermid', 'partition'])
+        stage_data = self._preprocessed_data.query('partition=="test"')
+        stage_data = stage_data.drop(columns=['orgpermid', 'partition'])
         self._stage = 4
-        print(f"stage 4 data provided - (n,X+y+geo_id)= {len(stage_data)}")
+        print(f"stage 4 data provided - (n,X+y+geo_id)= {stage_data.shape}")
         return stage_data
 
 ### ---------------------------------------------------------
